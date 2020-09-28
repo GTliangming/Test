11709164230927a3a8b80e238575582264bb9b79
liangming <liangming1@lightinthebox.com>
2020-09-28 19:28:31 +0800

    配置node的设置cookie

--- node/routes/index.js
+++ node/routes/index.js
@@ -1,6 +1,13 @@
 const router = require('koa-router')()
 
 router.get('/', async (ctx, next) => {
+  try{
+    ctx.session.username = "11";
+    ctx.session.age = "22";
+  }catch(e){
+    console.log(111111,e)
+  }
+  console.log(2222,ctx.session.username)
   await ctx.render('index', {
     title: 'Hello Koa 2!'
   })
