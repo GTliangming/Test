20bae6da6c6ce52848e91a96a1519584871be43e
liangming <liangming1@lightinthebox.com>
2020-10-06 22:56:19 +0800

    node update

--- node/routes/users.js
+++ node/routes/users.js
@@ -2,12 +2,17 @@ const router = require('koa-router')()
 
 router.prefix('/users')
 
-router.get('/', function (ctx, next) {
-  ctx.body = 'this is a users response!'
-})
 
-router.get('/bar', function (ctx, next) {
-  ctx.body = 'this is a users/bar response'
-})
+const {
+  login,
+  loginAdmin
+} = require('../controllers/user');
+
+
+
+
+router.post('/login',login)
+router.post('/loginAdmin',loginAdmin)
+
 
 module.exports = router
